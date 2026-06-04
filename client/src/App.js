16de76bb075f491725a06dd7a1923d7052489059
Manas Tripathi <manas.tri1111@gmail.com>
2026-06-04 15:06:43 +0530

    Added Sepolia deployment and React frontend

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { ethers } from "ethers";
 import contractData from "./Identity.json";
 
-const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const contractAddress = "0x29EfDc621e7df6a363571879C52684d6C177bFcA";
 const abi = contractData.abi;
 
 function App() {
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,13 @@
 require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.28",
+
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
